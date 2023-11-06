9d0b3d47ef8fdebbfcdcefa6b346bdf870ee90b6
theenathayalan97 <118962794+theenathayalan97@users.noreply.github.com>
2023-11-06 16:50:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ console.log('Server starting...');
 app.get('/',(req, res) => {
     console.log('Request received...');
     res.end('Hello GCP dev! in theena complete task-1');
-}).listen(8080, () => {
+}).listen(80, () => {
     console.log('started.');
 });
